770391428d783f5970d34ea6aee0f5226bd8b1ba
aditya.agrawal1 <aditya.agrawal@sjsu.edu>
2020-11-23 16:52:57 -0800

    initialize

--- app.py
+++ app.py
@@ -13,10 +13,18 @@ def index():
             profile = requests.get(
                 f'https://finnhub.io/api/v1/stock/profile2?symbol={symbol}&token=butkvvv48v6qj1ddtp10')
             stock = requests.get(f'https://finnhub.io/api/v1/quote?symbol={symbol}&token=butkvvv48v6qj1ddtp10')
+            change = stock.json()['c'] - stock.json()['pc']
+            current = stock.json()['c']
+            percent = (change/current) * 100
+
+            if change > 0: change = '+'+str(round(change, 2))
+            else: change = round(change, 2)
+            if change > 0: percent = '+'+str(round(percent,2))
+            else: percent = round(percent, 2)
             if profile.json() and stock.json():
-                return render_template('details.html', date=datetime.datetime.now(), name=profile.json()['name'],
-                                       ticker=profile.json()['ticker'], current=stock.json()['c'],
-                                       change=stock.json()['c'] - stock.json()['pc'])
+                return render_template('details.html', date=datetime.datetime.now().strftime("%a %b %d %H:%M:%S PDT %Y"), name=profile.json()['name'],
+                                       ticker=profile.json()['ticker'], current=round(current ,2),
+                                       change=change, percent=percent)
             else:
                 return render_template('error.html', message="Invalid Symbol")
         except:
